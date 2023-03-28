e9b7638ae328e04c07bb640bae27086810ebad01
Nicolás Boattini <38139882+nicolasboattini@users.noreply.github.com>
2023-03-28 12:01:34 -0300

    new assets

---
+++ Assets/RandomBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBackground : MonoBehaviour {
+    public Sprite[] mySprites;
+    public SpriteRenderer spriteRenderer;    
+
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = mySprites[0];
+    }
+
+    private void Start()
+    {
+        ChangeSprite();
+    }
+}
