f35f50edd4922c398ab906e2f71b81ef64aa3f9b
nicolasboattini <38139882+nicolasboattini@users.noreply.github.com>
2023-10-05 16:16:58 -0300

    Update CrearCartas.cs

--- Assets/Main/Scripts/CrearCartas.cs
+++ Assets/Main/Scripts/CrearCartas.cs
@@ -6,31 +6,22 @@ using UnityEngine.UI;
 public class CrearCartas : MonoBehaviour {
 
 	public GameObject CartaPrefab;
-	public int ancho;
     public int nivel;
     public int rows;
     public int cols;
 	public Transform CartasParent;
 	private List<GameObject> cartas = new List<GameObject> ();
-
 	public Material[] materiales;
 	public Texture2D[] texturas;
-
 	public int contadorClicks = 1;
 	public Text textoContadorIntentos;
-
 	public Carta CartaMostrada;
 	public bool sePuedeMostrar = true;
-
 	public InterfazUsuario interfazUsuario;
-
 	public int numParejasEncontradas;
-	
-
     public Camera camara;
     public GameObject fondo;
     public GameObject tablero;
-
     public AudioSource resultSound;
     public AudioClip m_correctSound = null;
     public AudioClip m_incorrectSound = null;
@@ -44,7 +35,7 @@ public class CrearCartas : MonoBehaviour {
         }
         else
         {
-            ancho = 0;
+            nivel = 0;
             cartas.Clear();
             GameObject[] cartasEli = GameObject.FindGameObjectsWithTag("Carta");
             for (int i = 0; i < cartasEli.Length; i++)
@@ -64,122 +55,79 @@ public class CrearCartas : MonoBehaviour {
         }
 	}
 
-    public void SetNivelFacil()
-	{
+    public void SetNivelFacil(){
 		nivel = 4;
 	}
-    public void SetNivelMedio()
-    {
+    public void SetNivelMedio(){
         nivel = 6;
     }
-    public void SetNivelDificil()
-    {
+    public void SetNivelDificil(){
         nivel = 8;
     }    
-    public void CrearParam()
-    {
-        //if (nivel == 0){
-            //print("Seleccione dificultad");
-        //} else
-        //{
-            
-            /*if (nivel == 4) // Si la dificultad es Fácil
-            {
-                // Mover la cámara a la posición central para la dificultad "Fácil"
-                Camera.main.transform.position = new Vector3(2.29f, 10.65f, 1.97f);
-                //-1.66 +1.85 -1.58
-                fondo.transform.position = new Vector3(2.29f, -3.45f, 1.97f);
-                tablero.transform.position = new Vector3(2.25f, -0.54f, 1.91f);
+    public void CrearParam(){
+        switch(nivel){
+            case 4:
+                rows = 3;
+                cols = 2;
+                Camera.main.transform.position = new Vector3(1f, 7.61f, 2.67f);
+            break;
+
+            case 6:
+                rows= 6;
+                cols= 3;
+                Camera.main.transform.position = new Vector3(1.37f, 9.22f, 4.03f);
+            break;
+
+            case 8:
+                rows=6;
+                cols=4;
+                Camera.main.transform.position = new Vector3(1.37f, 6.71f, 2.83f);
+            break;
+        }
+        int cont = 0;
+
+        for (int i = 0; i < rows; i++){
+            for (int x = 0; x < cols; x++){
+                float factor = 9.0f / nivel;
+                Vector3 posicionTemp = new Vector3((float)(x * (factor-0.2)), 0, (float)(i * (factor-0.2)));
+
+                GameObject cartaTemp = Instantiate(CartaPrefab, posicionTemp, Quaternion.Euler(new Vector3(0, 180, 0)));
+                cartaTemp.transform.localScale *= factor;
+                cartas.Add(cartaTemp);
+                cartaTemp.GetComponent<Carta>().posicionOriginal = posicionTemp;
+                //cartaTemp.GetComponent<Carta> ().idCarta = cont;
+                cartaTemp.transform.parent = CartasParent;
+                cont++;
             }
-            else
-            {
-                Camera.main.transform.position = new Vector3(3.95f, 8.8f, 3.55f);
-                fondo.transform.position = new Vector3(3.95f, -5.3f, 3.55f);
-                tablero.transform.position = new Vector3(3.91f, -2.11f, 3.49f);
-            }*/
-            switch(nivel){
-                case 4:
-                    rows = 3;
-                    cols = 2;
-                    Camera.main.transform.position = new Vector3(1f, 7.61f, 2.67f);
-                break;
-
-                case 6:
-                    rows= 6;
-                    cols= 3;
-                    Camera.main.transform.position = new Vector3(1.37f, 9.22f, 4.03f);
-                break;
-
-                case 8:
-                    rows=6;
-                    cols=4;
-                    Camera.main.transform.position = new Vector3(1.37f, 6.71f, 2.83f);
-                break;
-            }
-            int cont = 0;
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int x = 0; x < cols; x++)
-                {
-                    float factor = 9.0f / nivel;
-                    Vector3 posicionTemp = new Vector3((float)(x * (factor-0.2)), 0, (float)(i * (factor-0.2)));
-
-                    GameObject cartaTemp = Instantiate(CartaPrefab, posicionTemp,
-                        Quaternion.Euler(new Vector3(0, 180, 0)));
-
-                    cartaTemp.transform.localScale *= factor;
-
-                    cartas.Add(cartaTemp);
-
-                    cartaTemp.GetComponent<Carta>().posicionOriginal = posicionTemp;
-                    //cartaTemp.GetComponent<Carta> ().idCarta = cont;
-
-                    cartaTemp.transform.parent = CartasParent;
-
-                    cont++;
-                }
-            }
-            AsignarTexturas();
-            Barajar();
-        //}
-		
+        }
+        AsignarTexturas();
+        Barajar();
     }
 
     void AsignarTexturas(){
-
 		int[] arrayTemp =new int[texturas.Length];
-
-		for (int i = 0; i <= texturas.Length-1; i++) {
+		for (int i = 0; i <= texturas.Length-1; i++){
 			arrayTemp [i] = i;
 		}
-
-
-		for (int t = 0; t < arrayTemp.Length; t++ )
-		{
+		for (int t = 0; t < arrayTemp.Length; t++ ){
 			int tmp = arrayTemp[t];
 			int r = Random.Range(t, arrayTemp.Length);
 			arrayTemp[t] = arrayTemp[r];
 			arrayTemp[r] = tmp;
 		}
-
 		//int[] arrayDefinitivo = new int[ancho*ancho];
-		int[] arrayDefinitivo = new int[(rows*cols)/2];
-
-		for (int i = 0; i < arrayDefinitivo.Length ; i++) {
+		int[] arrayDefinitivo = new int[rows*cols/2];
+		for (int i = 0; i < arrayDefinitivo.Length ; i++){
 			arrayDefinitivo [i] = arrayTemp [i];
 		}
-
-
 		for(int i=0;i<cartas.Count ;i++){
-			cartas[i].GetComponent<Carta> ().AsignarTextura (texturas[(arrayDefinitivo[i/2])] );
+			cartas[i].GetComponent<Carta> ().AsignarTextura (texturas[arrayDefinitivo[i/2]] );
 			cartas [i].GetComponent<Carta> ().idCarta = i / 2;
 		}
 	}
 
 	void Barajar(){
 		int aleatorio;
-
 		for (int i = 0; i < cartas.Count; i++) {
 			aleatorio = Random.Range (i, cartas.Count);
 
@@ -248,18 +196,15 @@ public class CrearCartas : MonoBehaviour {
 		textoContadorIntentos.text = "" + contadorClicks;
 	}
 
-		/*public void ProporcionarCartas(){
-			if (numCartas % 2 == 0) {
-				if (numCartas % 6 == 0) {
-					ancho = 6;
-					alto=numCartas / 6;
-				}
-	
-				Crear ();
-	
+	/*public void ProporcionarCartas(){
+		if (numCartas % 2 == 0) {
+			if (numCartas % 6 == 0) {
+				ancho = 6;
+				alto=numCartas / 6;
+			}	
+			Crear ();
 			} else {
 				print ("Numero de cartas debe ser par");
 			}
-	
-		}*/
+	}*/
 }
